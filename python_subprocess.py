446cf7270ec401ceb2f293403decebb4bc296d78
SupidchayaTangkidwanich <linzpeach@kkumail.com>
2023-02-20 11:53:24 +0700

    hw

--- python_subprocess.py
+++ python_subprocess.py
@@ -10,6 +10,16 @@ if __name__ == "__main__":
     out, err = process1.communicate()
     print(out.decode('utf-8'))
 
+    sum = 0 
+    count = 0 
+    for i in range(100):
+        if i % 2 != 0 : #หาร2แล้วไม่เท่ากับ 0
+            count +=1
 
+            process1 = subprocess.Popen(["python", "python_script101.py", "--x",f'{i}'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = process1.communicate()1
+            print(out.decode('utf-8'))
+            
+            sum += (i*2)
     #HW สั่งรัน python_script101.py 50 รอบ โดย x = 1,3,5,7,... 
     #แล้วให้เอา output ของ xt มารวมกัน แล้ว print ออกมา
