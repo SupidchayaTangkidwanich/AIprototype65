21cd52b1a28017ac7b2cae8b36031342c0aa31ab
SupidchayaTangkidwanich <linzpeach@kkumail.com>
2023-02-20 14:44:50 +0700

    postrequest

---
+++ postrequest.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+if __name__ == "__main__":
+
+    url = 'http://172.30.254.222:5001/postrequest'
+    myobj = {'Supidchaya': 'Supidchaya'}
+
+    x = requests.post(url, data = json.dumps(myobj))
+
+    print(x.text)
